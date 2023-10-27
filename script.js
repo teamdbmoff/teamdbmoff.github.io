b299dec225b273afc0bef37773103f39f3f03215
teamdbmoff <149051728+teamdbmoff@users.noreply.github.com>
2023-10-27 18:25:26 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+function loadContent(section) {
+    var contentDiv = document.getElementById('content');
+  
+    if (section === 'home') {
+      contentDiv.innerHTML = `
+        <div id="home">
+          <h1>Welkom op de Homepagina</h1>
+          <p>Dit is de inhoud van de homepagina.Voeg hier je eigen tekst toe.</p>
+        </div>
+      `;
+    } else if (section === 'binairstelsel') {
+      contentDiv.innerHTML = `
+        <div id="binairstelsel">
+          <h1>Binairstelsel</h1>
+          <p>Dit is de inhoud van de sectie over het binairstelsel. Voeg hier je eigen tekst toe.</p>
+        </div>
+      `;
+    } else if (section === 'kleurmodellen') {
+      contentDiv.innerHTML = `
+        <div id="kleurmodellen">
+          <h1>Kleurmodellen</h1>
+          <p>Dit is de inhoud van de sectie over kleurmodellen. Voeg hier je eigen tekst toe.</p>
+        </div>
+      `;
+    }
+  }
+  
